26dc6e4a55a0ef210cfc1676c73ff0292194d0b2
JBar <79889472+JBar-mmmmm@users.noreply.github.com>
2022-09-17 22:09:17 +1000

    Wk6 Demo box spawning works (yay)
    just needs a crash screen & reset
    maybe some colours

--- week6.js
+++ week6.js
@@ -13,22 +13,16 @@ let y = 0; //relative y pos of ship from initial
 let xMod = 2; //ship x vel mod
 let yMod = 2; //ship y vel mod
 let zMod = 1; //box displacement rate mod
-let boxSize = 50;
+let boxSize = 50; //50
 let hitboxMod = 4 / 5; //box hitbox size mod
 
 //randomise box x position on spawn
-let xValue0Array = [50, 375, 500];
-let xValue1Array = [150, 475, 600];
-let xValue2Array = [250, 575, 700];
-let xValue0Random = 150;
-
-
 let boxPos = {
-    x: [xValue0Random, 300, 600], //x position
+    x: [150, 300, 600], //x position
     y: [-50, -300, 0], //y starting position (staggered for different timings)
     z: [0, 0, 0], //y displacement 
+    xRespawn: [[50, 200, 350, 500, 650], [100, 250, 400, 550, 700], [150, 300, 450, 600]] //possible x pos for each respawning box
 }
-
 
 function draw() {
     ship();
@@ -48,6 +42,8 @@ function ship() {
                 yMod = yMod + 1 / 5;
                 zMod = zMod + 2 / 5;
             }
+            boxPos.x[i] = random(boxPos.xRespawn[i]); //randomised box x pos on respawn
+            boxPos.y[i] = random(-200, -20); //randomised box y pos on repsawn
         }
         boxPos.z[i] = boxPos.z[i] + 1 * zMod;
     }
