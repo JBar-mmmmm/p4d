3d63378df3657060b7b4c868ad0f233f568484a9
JBar <79889472+JBar-mmmmm@users.noreply.github.com>
2022-09-18 02:18:17 +1000

    wk6 demo finished

--- week6.js
+++ week6.js
@@ -1,6 +1,11 @@
 /*
     Paste the code for your week 6 exercise below.
 */
+
+function preload() {
+    scoreFont = loadFont('https://dl.dropboxusercontent.com/s/mmipx1p7oiuy7th/articulat-cf-demi-bold.ttf?dl=0');
+}
+
 function setup() {
     createCanvas(750, 750);
     frameRate(60);
@@ -12,9 +17,10 @@ let x = 0; //relative x pos of ship from initial
 let y = 0; //relative y pos of ship from initial
 let xMod = 2; //ship x vel mod
 let yMod = 2; //ship y vel mod
-let zMod = 1; //box displacement rate mod
+let zMod = 2; //box displacement rate mod
 let boxSize = 50; //50
 let hitboxMod = 4 / 5; //box hitbox size mod
+let scoreFont;
 let score = 0;
 let lastScore = 0;
 let highScore = 0;
@@ -37,13 +43,15 @@ function ship() {
     //boxes & game speed
     rectMode(CENTER);
     for(i = 0; i < 3; i = i + 1) {
+        noStroke();
+        fill('#444');
         rect(boxPos.x[i], boxPos.y[i] + boxPos.z[i], boxSize);
         if (boxPos.z[i] > 800 - boxPos.y[i]) {
             boxPos.z[i] = 0;
             if (zMod < 10) { //speed control
                 xMod = xMod + 1 / 5;
                 yMod = yMod + 1 / 5;
-                zMod = zMod + 2 / 5;
+                zMod = zMod + 4 / 5;
             }
             boxPos.x[i] = random(boxPos.xRespawn[i]); //randomised box x pos on respawn
             boxPos.y[i] = random(-200, -20); //randomised box y pos on repsawn
@@ -52,13 +60,18 @@ function ship() {
         boxPos.z[i] = boxPos.z[i] + 1 * zMod;
     }
 
-    //score display
-    text('High Score ' + highScore, 50, 30);
-    text('Last Score ' + lastScore, 50, 40);
-    text('Score ' + score, 50, 50);
-
     //ship
+    stroke(0);
+    fill('#FFF');
     triangle(initialX + x, initialY + y, initialX + 15 + x, initialY + 30 + y, initialX - 15 + x, initialY + 30 + y);
+
+    //score display
+    fill('#000');
+    textFont(scoreFont);
+    textSize(20);
+    text('High Score ' + highScore, 50, 50);
+    text('Last Score ' + lastScore, 50, 72);
+    text('Score ' + score, 50, 104);
 
     //movement
     if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
@@ -77,7 +90,7 @@ function ship() {
         }
     }
     if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
-        if (y < 20) {
+        if (y < 15) {
             y = y + 1 * yMod;
         }
     }
@@ -85,7 +98,6 @@ function ship() {
     //collision checker
     for(i = 0; i < 3; i = i + 1) {
         if (x < boxPos.x[i] - initialX + boxSize * hitboxMod && x > boxPos.x[i] - initialX - boxSize * hitboxMod && y + 15 < boxPos.y[i] - initialY + boxSize * hitboxMod + boxPos.z[i] && y + 15 > boxPos.y[i] - initialY - boxSize * hitboxMod + boxPos.z[i]) { 
-            //reset
             reset();
         }
     }
@@ -96,7 +108,7 @@ function reset() {
     y = 0;
     xMod = 2;
     yMod = 2;
-    zMod = 1;
+    zMod = 2;
     lastScore = score;
     if (lastScore > highScore) {
         highScore = lastScore;
